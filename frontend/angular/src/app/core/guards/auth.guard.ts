cdef897c7a17097118f3b83533fddb0d3abb9ee9
grollick <61632946+grollick@users.noreply.github.com>
2026-01-25 20:50:25 +0100

    fix jwt issuer-uri

--- frontend/angular/src/app/core/guards/auth.guard.ts
+++ frontend/angular/src/app/core/guards/auth.guard.ts
@@ -20,17 +20,3 @@ export const authGuard: CanActivateFn = async (route, state) => {
   return false;
 };
 
-/*
-export const authGuard: CanActivateFn = async (route, state) => {
-  const keycloak = inject(KeycloakService);
-
-  if (keycloak.isLoggedIn()) {
-    return true;
-  }
-
-  await keycloak.login({
-    redirectUri: window.location.origin + state.url
-  });
-
-  return false;
-};*/
