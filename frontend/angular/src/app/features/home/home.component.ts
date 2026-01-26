417fdce9b8c8a2dc1f544bac7d6e8e5967f68cdf
grollick <61632946+grollick@users.noreply.github.com>
2026-01-26 18:46:30 +0100

    homepage

--- frontend/angular/src/app/features/home/home.component.ts
+++ frontend/angular/src/app/features/home/home.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {MatButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardAvatar,
+  MatCardContent,
+  MatCardHeader,
+  MatCardTitle
+} from '@angular/material/card';
+import {RouterLink} from '@angular/router';
+import {AuthService} from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatButton,
+    MatIcon,
+    MatCardActions,
+    MatCardContent,
+    MatCardAvatar,
+    MatCardTitle,
+    MatCardHeader,
+    MatCard,
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  auth = inject(AuthService);
+
+  scrollToFeatures() {
+    document.getElementById('vision-section')?.scrollIntoView({ behavior: 'smooth' });
+  }
 
 }
