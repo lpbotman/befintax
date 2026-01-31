69e5f2997143255c09192725f6f20f6882a1505f
grollick <61632946+grollick@users.noreply.github.com>
2026-01-31 15:00:52 +0100

    market history fallback + yahoo provider

--- frontend/angular/src/app/features/common/asset-chart/asset-chart.component.ts
+++ frontend/angular/src/app/features/common/asset-chart/asset-chart.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
+import {ChangeDetectorRef, Component, effect, input, Input, ViewChild} from '@angular/core';
 import { ChartConfiguration, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { CommonModule } from '@angular/common';
 import 'chartjs-adapter-date-fns';
 import {MarketDataApiService} from '../../../core/services/market-data-api.service';
-import {AssetType} from '../../../core/models/asset.model'; // Important pour les dates !
+import {Asset} from '../../../core/models/asset.model';
+
 
 @Component({
   selector: 'app-asset-chart',
@@ -13,12 +14,13 @@ import {AssetType} from '../../../core/models/asset.model'; // Important pour le
   templateUrl: './asset-chart.component.html',
   styleUrls: ['./asset-chart.component.scss']
 })
-export class AssetChartComponent implements OnChanges {
-  @Input() symbol: string = '';
-  @Input() assetType: string = 'STOCK';
-  @Input() exchange: string = '';
+export class AssetChartComponent {
+  asset = input<Asset>()
   @Input() label: string = 'Évolution du cours';
-  @Input() color: string = '#4F46E5'; // Une belle couleur Indigo par défaut
+  @Input() color: string = '#4F46E5';
+
+  currentAsset?: Asset;
+  isLoadingChart = true;
 
   @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
 
@@ -52,22 +54,28 @@ export class AssetChartComponent implements OnChanges {
 
   public lineChartType: ChartType = 'line';
 
-  constructor(private marketService: MarketDataApiService) {}
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['symbol'] && this.symbol) {
-      this.loadData();
-    }
+  constructor(private cdr: ChangeDetectorRef, private marketService: MarketDataApiService) {
+    effect(() => {
+      const asset = this.asset();
+      if(asset && asset.symbol !== this.currentAsset?.symbol) {
+        this.currentAsset = asset;
+        this.loadData();
+      }
+    });
   }
 
   private loadData(): void {
-    this.marketService.getHistory(this.symbol, this.assetType, this.exchange).subscribe({
+    const asset = this.asset();
+    if (!asset) return;
+
+    this.isLoadingChart = true;
+
+    this.marketService.getHistory(asset.symbol!, asset.type!, asset.exchange).subscribe({
       next: (data) => {
-        // Préparation des données pour Chart.js
         this.lineChartData = {
           datasets: [{
             data: data.map(p => p.price),
-            label: this.symbol,
+            label: asset.symbol,
             borderColor: this.color,
             backgroundColor: this.createGradient(this.color),
             fill: true, // Effet "Area Chart" rempli
@@ -75,8 +83,8 @@ export class AssetChartComponent implements OnChanges {
           }],
           labels: data.map(p => p.date)
         };
-
-        // Force le rafraîchissement
+        this.isLoadingChart = false;
+        this.cdr.detectChanges();
         this.chart?.update();
       },
       error: (err) => console.error('Erreur chargement graphique', err)
